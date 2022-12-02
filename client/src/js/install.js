1b83dc8c57c874bd9dc6da7fdcb891c2d88c6b61
Omar <omar.o454@gmail.com>
2022-12-02 01:22:34 -0700

    node module issue

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -20,6 +20,7 @@ butInstall.addEventListener('click', async () => {
 
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
-});
+window.addEventListener("appinstalled", () => {
+    butInstall.classList.toggle("hidden", true);
+    deferredPrompt = null;
+  });
